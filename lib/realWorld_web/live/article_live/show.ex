9a2e6c7400891e101af00ce5b8185835d76ce58c
mark0802-prog <hayabusa.deu.horn@live.jp>
2022-02-27 18:51:31 +0900

    コメント機能の実装

--- lib/realWorld_web/live/article_live/show.ex
+++ lib/realWorld_web/live/article_live/show.ex
@@ -7,14 +7,18 @@ defmodule RealWorldWeb.ArticleLive.Show do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, :comment_changeset, change_comment())}
   end
 
   @impl true
   def handle_params(%{"id" => id}, _, socket) do
-    article = Blogs.get_article!(id)
-    %{live_action: action, current_user: user} = socket.assigns
+    article = get_article!(id)
     author_id = article.author_id
+
+    %{
+      live_action: action,
+      current_user: user
+    } = socket.assigns
 
     if action == :edit && user.id != author_id do
       {:noreply,
@@ -42,6 +46,39 @@ defmodule RealWorldWeb.ArticleLive.Show do
       {:ok, _} = Blogs.delete_article(article)
       {:noreply, push_redirect(socket, to: Routes.article_index_path(socket, :index))}
     end
+  end
+
+  @impl true
+  def handle_event("post_comment", %{"comment" => comment_params}, socket) do
+    case create_comment(comment_params, socket) do
+      {:ok, _} ->
+        article = get_article!(socket.assigns.article.id)
+
+        {:noreply,
+         socket
+         |> assign(:article, article)
+         |> assign(:comment_changeset, change_comment())}
+
+      {:error, changeset} ->
+        {:norepl, assign(socket, :comment_changeset, changeset)}
+        nil
+    end
+  end
+
+  defp get_article!(id), do: Blogs.get_article!(id) |> RealWorld.Repo.preload(:comments)
+
+  defp change_comment, do: Blogs.change_comment(%Blogs.Comment{})
+
+  defp create_comment(comment_params, socket) do
+    %{
+      article: article,
+      current_user: user
+    } = socket.assigns
+
+    comment_params
+    |> Map.put("article_id", article.id)
+    |> Map.put("author_id", user.id)
+    |> Blogs.create_comment()
   end
 
   defp page_title(:show), do: "Show Article"
