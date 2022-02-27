abb1eed2c00220a5fd365b1b24082184ebafd26b
mark0802-prog <hayabusa.deu.horn@live.jp>
2022-02-27 17:55:56 +0900

    詳細画面から記事を削除する機能を追加

--- lib/realWorld_web/live/article_live/show.ex
+++ lib/realWorld_web/live/article_live/show.ex
@@ -29,6 +29,21 @@ defmodule RealWorldWeb.ArticleLive.Show do
     end
   end
 
+  @impl true
+  def handle_event("delete", _value, socket) do
+    %{
+      article: article,
+      current_user: user
+    } = socket.assigns
+
+    if article.author_id != user.id do
+      {:noreply, put_flash(socket, :error, "You can't delete this article.")}
+    else
+      {:ok, _} = Blogs.delete_article(article)
+      {:noreply, push_redirect(socket, to: Routes.article_index_path(socket, :index))}
+    end
+  end
+
   defp page_title(:show), do: "Show Article"
   defp page_title(:edit), do: "Edit Article"
 end
